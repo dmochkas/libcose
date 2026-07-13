1f924722256d524d63fb004c466777293a61794c
Dmytro Ochkas <dmytro.ochkas@imt-atlantique.fr>
2026-07-14 00:26:00 +0200

    Implement hkdf with Ascon-Hash256

--- include/cose/crypto/ascon_c.h
+++ include/cose/crypto/ascon_c.h
@@ -17,6 +17,15 @@ extern "C" {
 #define COSE_CRYPTO_AEAD_ASCON_64_ABYTES        8
 #define COSE_CRYPTO_AEAD_ASCON_32_ABYTES        4
 
+#define ASCON_HASH_SIZE 32
+#define ASCON_HMAC_BLOCK_SIZE 32
+
+typedef union {
+    uint64_t x[5];
+    uint32_t w[5][2];
+    uint8_t b[5][8];
+} ascon_state_t;
+
 int ascon_aead_encrypt_trunc(uint8_t* t, uint8_t* c, const uint8_t* m, uint64_t mlen,
                        const uint8_t* ad, uint64_t adlen, const uint8_t* npub,
                        const uint8_t* k, uint16_t t_bits);
@@ -24,6 +33,10 @@ int ascon_aead_encrypt_trunc(uint8_t* t, uint8_t* c, const uint8_t* m, uint64_t 
 int ascon_aead_decrypt_trunc(uint8_t* m, const uint8_t* t, const uint8_t* c,
                        uint64_t clen, const uint8_t* ad, uint64_t adlen,
                        const uint8_t* npub, const uint8_t* k, uint16_t t_bits);
+
+void ascon_inithash(ascon_state_t* s);
+void ascon_absorb(ascon_state_t* s, const uint8_t* in, uint64_t inlen);
+void ascon_squeeze(ascon_state_t* s, uint8_t* out, uint64_t outlen);
 
 #ifdef __cplusplus
 }
--- src/crypt/ascon_c.c
+++ src/crypt/ascon_c.c
@@ -1,6 +1,11 @@
 #include "cose/crypto.h"
 #include "cose/crypto/ascon_c.h"
 
+#include <string.h>
+
+// TODO: Find better solution
+#define secure_memzero(mem, len) memset(mem, 0, len)
+
 static int ascon_aead_get_tag_size(cose_algo_t algo, uint16_t* t_bits, uint16_t* t_bytes) {
     switch (algo) {
         case COSE_ALGO_ASCON_AEAD128:
@@ -90,4 +95,161 @@ int cose_crypto_aead_decrypt_ascon(uint8_t *msg,
 
     *msglen = clen - t_bytes;
     return 0;
+}
+
+/********************** Hash ************************/
+
+typedef struct
+{
+    ascon_state_t inner;
+    ascon_state_t outer;
+} crypto_auth_hmacascon256_state;
+
+static int crypto_auth_hmacascon256_init(
+    crypto_auth_hmacascon256_state *state,
+    const uint8_t *key,
+    size_t keylen)
+{
+    uint8_t k0[ASCON_HMAC_BLOCK_SIZE];
+    uint8_t ipad[ASCON_HMAC_BLOCK_SIZE];
+    uint8_t opad[ASCON_HMAC_BLOCK_SIZE];
+
+    memset(k0, 0, sizeof(k0));
+
+    if (keylen > ASCON_HMAC_BLOCK_SIZE) {
+
+        ascon_state_t tmp;
+
+        ascon_inithash(&tmp);
+        ascon_absorb(&tmp, key, keylen);
+        ascon_squeeze(&tmp, k0, ASCON_HASH_SIZE);
+
+        memset(&tmp,0,sizeof(tmp));
+    } else {
+
+        memcpy(k0, key, keylen);
+    }
+
+    for (size_t i = 0; i < ASCON_HMAC_BLOCK_SIZE; i++) {
+        ipad[i] = k0[i] ^ 0x36;
+        opad[i] = k0[i] ^ 0x5c;
+    }
+
+    ascon_inithash(&state->inner);
+    ascon_absorb(&state->inner, ipad, ASCON_HMAC_BLOCK_SIZE);
+
+    ascon_inithash(&state->outer);
+    ascon_absorb(&state->outer, opad, ASCON_HMAC_BLOCK_SIZE);
+
+    secure_memzero(k0,sizeof(k0));
+    secure_memzero(ipad,sizeof(ipad));
+    secure_memzero(opad,sizeof(opad));
+
+    return 0;
+}
+
+static int crypto_auth_hmacascon256_update(
+    crypto_auth_hmacascon256_state *state,
+    const uint8_t *msg,
+    size_t msglen)
+{
+    ascon_absorb(&state->inner, msg, msglen);
+    return 0;
+}
+
+static int crypto_auth_hmacascon256_final(
+    crypto_auth_hmacascon256_state *state,
+    uint8_t *out)
+{
+    uint8_t inner_hash[ASCON_HASH_SIZE];
+
+    ascon_squeeze(&state->inner,
+                  inner_hash,
+                  ASCON_HASH_SIZE);
+
+    ascon_absorb(&state->outer,
+                 inner_hash,
+                 ASCON_HASH_SIZE);
+
+    ascon_squeeze(&state->outer,
+                  out,
+                  ASCON_HASH_SIZE);
+
+    secure_memzero(inner_hash,sizeof(inner_hash));
+    secure_memzero(state,sizeof(*state));
+
+    return 0;
+}
+
+int cose_crypto_hkdf_derive_ascon256(const uint8_t *salt, size_t salt_len,
+                                   const uint8_t *ikm, size_t ikm_length,
+                                   const uint8_t *info, size_t info_length,
+                                   uint8_t *out, size_t out_length) {
+    uint8_t prk[ASCON_HASH_SIZE];
+    uint8_t slice[ASCON_HASH_SIZE];
+    uint8_t counter = 1;
+    size_t slice_len = ASCON_HASH_SIZE;
+    size_t rounds;
+    crypto_auth_hmacascon256_state state;
+
+    /* ---------- HKDF-Extract ---------- */
+
+    crypto_auth_hmacascon256_init(&state, salt, salt_len);
+    crypto_auth_hmacascon256_update(&state, ikm, ikm_length);
+    crypto_auth_hmacascon256_final(&state, prk);
+
+    /* ---------- HKDF-Expand ---------- */
+
+    rounds = out_length / ASCON_HASH_SIZE;
+    if (out_length % ASCON_HASH_SIZE != 0) {
+        rounds++;
+    }
+
+    if (rounds > 255) return COSE_ERR_CRYPTO;
+
+    for (size_t i = 0; i < rounds; ++i) {
+        size_t offset = i * ASCON_HASH_SIZE;
+
+        counter = (uint8_t)(i + 1);
+
+        crypto_auth_hmacascon256_init(
+            &state,
+            prk,
+            sizeof(prk));
+
+        if (i > 0) {
+            crypto_auth_hmacascon256_update(
+                &state,
+                slice,
+                slice_len);
+        }
+
+        if (info != NULL && info_length > 0) {
+            crypto_auth_hmacascon256_update(
+                &state,
+                info,
+                info_length);
+        }
+
+        crypto_auth_hmacascon256_update(
+            &state,
+            &counter,
+            1);
+
+        crypto_auth_hmacascon256_final(
+            &state,
+            slice);
+
+        if (i + 1 == rounds) {
+            slice_len = out_length - offset;
+        }
+
+        memcpy(out + offset, slice, slice_len);
+    }
+
+    secure_memzero(prk, sizeof(prk));
+    secure_memzero(slice, sizeof(slice));
+    secure_memzero(&state, sizeof(state));
+
+    return COSE_OK;
 }
